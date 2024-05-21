596667bb9712939265ed058f0e1f8ef7008440a4
Federico De Marines <federicodemarines@MBP-de-Federico.fibertel.com.ar>
2024-05-21 15:53:08 -0300

    udpate

--- react/human-task-material-renderers/vite.config.ts
+++ react/human-task-material-renderers/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 import path from "path";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require("./package.json");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
@@ -15,16 +18,14 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/components/index.ts"),
       name: "human-components",
+      formats: ["es"],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
+      external: [
+        ...Object.keys(packageJson.devDependencies),
+        ...Object.keys(packageJson.peerDependencies),
+      ],
     },
     //Generates sourcemaps for the built files,
     //aiding in debugging.
