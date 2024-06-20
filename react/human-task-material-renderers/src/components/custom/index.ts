dd510077745f6042a0c049d6852ea0421279ee83
Najeeb <najeebthangalkp@gmail.com>
2024-06-20 14:06:01 +0530

    feat:Add RadioGroup custom component

--- react/human-task-material-renderers/src/components/custom/index.ts
+++ react/human-task-material-renderers/src/components/custom/index.ts
@@ -1,16 +1,15 @@
 /*
-* Copyright 2024 Conductor Authors.
-* <p>
-* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
-* the License. You may obtain a copy of the License at
-* <p>
-* http://www.apache.org/licenses/LICENSE-2.0
-* <p>
-* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-* an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations under the License.
-*/
-
+ * Copyright 2024 Conductor Authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 
 import DescriptionTextControl from "./DescriptionTextControl";
 import DescriptionTextTester from "./DescriptionTextTester";
@@ -24,6 +23,8 @@ import MarkdownTextControl from "./MarkdownText/MarkdownTextControl";
 import MarkdownTextTester from "./MarkdownText/MarkdownTextTester";
 import FileUploadControl from "./FileUploadControl";
 import FileUploadTester from "./FileUploadTester";
+import RadioGroupTester from "./RadioGroup/RadioGroupTester";
+import RadioGroupControl from "./RadioGroup/RadioGroupControl";
 
 import {
   materialCells,
@@ -37,6 +38,7 @@ export {
   BooleanControl,
   MarkdownTextControl,
   FileUploadControl,
+  RadioGroupControl,
 };
 export {
   DescriptionTextTester,
@@ -45,6 +47,7 @@ export {
   BooleanTester,
   MarkdownTextTester,
   FileUploadTester,
+  RadioGroupTester,
 };
 
 export const humanTaskCells = materialCells;
@@ -57,4 +60,5 @@ export const humanTaskRenderers = [
   { tester: BooleanTester, renderer: BooleanControl },
   { tester: MarkdownTextTester, renderer: MarkdownTextControl },
   { tester: FileUploadTester, renderer: FileUploadControl },
+  { tester: RadioGroupTester, renderer: RadioGroupControl },
 ];
